8163f56fb83cc0208a053b97eaf176b188a6fe7c
Ramkrishna1998 <ramkrishna@supertax.in>
2022-01-05 17:05:53 +0530

    initial commit

---
+++ reduxtest.js
@@ -0,0 +1,31 @@
+const { createStore } = require('redux');
+
+
+const initialState = {
+    age:21
+}; 
+
+const myReducer = (state = initialState, action) => {
+    const newState = {...state};
+     
+    if(action.type === "ADD"){
+        newState.age += action.val;
+    }
+    if(action.type === "SUBSTRACT"){
+        newState.age -= action.val;
+    }
+
+    return newState;
+}
+
+const store = createStore(myReducer);
+
+store.subscribe(() => {
+
+    console.log(' State Changes', JSON.stringify( store.getState()));
+})
+
+store.dispatch({type:'ADD', val:10});
+store.dispatch({type:'ADD',val:5});
+store.dispatch({type:'SUBSTRACT', val:3});
+
